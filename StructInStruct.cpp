c469615466150e0b57a075902963907071ba13de
abiiiaja <kalindraaraya@gmail.com>
2026-04-07 15:12:20 +0700

    Mengisi obyeknya

--- StructInStruct.cpp
+++ StructInStruct.cpp
@@ -11,3 +11,18 @@ struct orang{
     string alamat;
     int umur;
 };
+
+int main(){
+    orang mhs;
+
+    cout << "Nama Mahasiswa: ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa: ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur: ";
+    cin >> mhs.umur;
+    cout << endl;
+
+}
